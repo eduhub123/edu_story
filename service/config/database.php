71fcf3f6ad62e97d214af0efe1764dd007001f51
haild96 <hai.ledinh.hust@gmail.com>
2022-03-17 22:53:44 +0700

    update

--- service/config/database.php
+++ service/config/database.php
@@ -33,22 +33,6 @@ $db = [
 
     'connections' =>
         [
-//            'edu_story' => [
-//                'driver'      => 'mysql',
-//                'host'        => env('DB_HOST', '127.0.0.1'),
-//                'port'        => env('DB_PORT', 3306),
-//                'database'    => env('DB_DATABASE', 'edu_story'),
-//                'username'    => env('DB_USERNAME', ''),
-//                'password'    => env('DB_PASSWORD', 'monkeyapp@1234!@!'),
-//                'unix_socket' => env('DB_SOCKET', ''),
-//                'charset'     => env('DB_CHARSET', 'utf8mb4'),
-//                'collation'   => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-//                'prefix'      => env('DB_PREFIX', ''),
-//                'strict'      => env('DB_STRICT_MODE', true),
-//                'engine'      => env('DB_ENGINE', null),
-//                'timezone'    => env('DB_TIMEZONE', '+00:00'),
-//            ],
-
             'edu_story' => [
                 'read'        => [
                     'host'     => [
